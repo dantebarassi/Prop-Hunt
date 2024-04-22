740e39ef4eb2c5b7264cbdcd45871d26a1bb70ab
Juli-ui <57135670+Juli-ui@users.noreply.github.com>
2024-04-21 22:13:54 -0300

    Mañana hacer con rigid

--- Assets/Scripts/PlayerMovementSimple.cs
+++ Assets/Scripts/PlayerMovementSimple.cs
@@ -6,6 +6,9 @@ using Fusion;
 public class PlayerMovementSimple : NetworkBehaviour
 {
     public float speed=3;
+
+    private float _moveY;
+    private float _moveX;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +18,13 @@ public class PlayerMovementSimple : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!HasStateAuthority) return;
-            transform.position += (Vector3.up * Input.GetAxisRaw("Horizontal")) * speed * Time.deltaTime;
+        _moveY = Input.GetAxisRaw("Horizontal");
+        _moveX = Input.GetAxisRaw("Vertical");
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        transform.position += (Vector3.forward * _moveY) * speed * Runner.DeltaTime;
+        transform.position += (Vector3.right * _moveX*-1) * speed * Runner.DeltaTime;
     }
 }
