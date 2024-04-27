891fb0a92cbab4080c47c498660fa2dfb1277528
Juli-ui <57135670+Juli-ui@users.noreply.github.com>
2024-04-26 21:48:09 -0300

    b

--- Assets/Scripts/PlayerMovementSimple.cs
+++ Assets/Scripts/PlayerMovementSimple.cs
@@ -5,7 +5,7 @@ using Fusion;
 
 public class PlayerMovementSimple : NetworkBehaviour
 {
-    public float speed=3;
+    public float speed = 3;
 
     private float _moveY;
     private float _moveX;
@@ -14,18 +14,26 @@ public class PlayerMovementSimple : NetworkBehaviour
     private bool _jumpPressed;
     private bool _changeFormPressed;
     private Vector3 velocity;
-    private MeshRenderer meshRenderer;
-    private Collider collider;
 
+    private Vector3 cameraDir;
+    PlayerMovementSimple PlayerMovement;
+    Camera Camera;
+    private Renderer _renderer;
 
+    [Networked, OnChangedRender(nameof(NetChangeForm))]
+    public MeshRenderer netMeshRenderer { get; set; }
+    public Collider netCollider { get; set; }
+    public MeshRenderer OtherMeshRenderer;
+    public Collider OtherCollider;
     //Donde va?
     //Quaternion cameraRotationY = Quaternion.Euler(0, Camera.transform.rotation.eulerAngles.y, 0);
     //Vector3 move = cameraRotationY * new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Runner.DeltaTime * PlayerSpeed;
-
+    //Lo de aca hace que le avise a todos qeu tiene que cambiar ese variable 
+    public void NetChangeForm() => netMeshRenderer = OtherMeshRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     public override void Spawned()
@@ -34,6 +42,8 @@ public class PlayerMovementSimple : NetworkBehaviour
         _rb = GetComponent<Rigidbody>();
         if (!HasStateAuthority) return;
         speed = 15;
+        PlayerMovement = this;
+        Camera = Camera.main;
         Camera.main.GetComponent<CameraBehavior>().target = transform;
         //var velocity = _rb.velocity;
     }
@@ -41,9 +51,10 @@ public class PlayerMovementSimple : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!HasStateAuthority) return;
         _moveY = Input.GetAxisRaw("Horizontal");
         _moveX = Input.GetAxisRaw("Vertical");
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             _jumpPressed = true;
         }
@@ -53,6 +64,14 @@ public class PlayerMovementSimple : NetworkBehaviour
         if (Input.GetKeyDown(KeyCode.E))
         {
             _changeFormPressed = true;
+        }
+        //Raycast
+        Ray ray = PlayerMovement.Camera.ScreenPointToRay(Input.mousePosition);
+        ray.origin += PlayerMovement.Camera.transform.forward;
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Debug.DrawRay(ray.origin, ray.direction, Color.red, 1f);
         }
     }
 
@@ -71,14 +90,17 @@ public class PlayerMovementSimple : NetworkBehaviour
     public void Movement()
     {
         if (_jumpPressed) Jump();
-        if (_changeFormPressed) ChangeForm(meshRenderer,collider);
+        if (_changeFormPressed) ChangeForm(OtherMeshRenderer, OtherCollider);
 
         if (_moveY != 0 || _moveX != 0)
         {
-            _rb.velocity += (((Vector3.forward * _moveY) * speed * Runner.DeltaTime) + ((Vector3.right * _moveX * -1) * speed * Runner.DeltaTime)) * -1;
+            cameraDir = Camera.main.transform.forward;
+            cameraDir.y = 0;
+            this.transform.forward = cameraDir;
+            _rb.velocity += (((this.transform.forward * _moveX) * speed * Runner.DeltaTime) + ((this.transform.right * _moveY) * speed * Runner.DeltaTime));
             if (Mathf.Abs(_rb.velocity.magnitude) > speed)
             {
-                var velocity = Vector3.ClampMagnitude(_rb.velocity, speed);
+                //var velocity = Vector3.ClampMagnitude(_rb.velocity, speed);
                 velocity.y = _rb.velocity.y;
                 _rb.velocity = velocity;
             }
